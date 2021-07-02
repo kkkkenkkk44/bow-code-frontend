c464c6df1899bb111eac606a8c920038e1d646cf
jhow <jhow@ntu.im>
2021-07-02 17:13:05 +0800

    Modify Course List page layout

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,27 +1,35 @@
 import React from "react"
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-class CourseCard extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+export default function CourseCard(props) {
+    const useStyles = makeStyles((theme) => ({
+        title: {
 
-    render() {
-        var abstract = this.props.course.abstract
-        if (abstract.length > 80) {
-            abstract = abstract.substring(0, 80) + "..."
+        },
+        abstract: {
+            color: "#707070"
         }
-        return (
-            <Card>
-                <CardContent>
-                    <h4>{this.props.course.name}</h4>
+    }));
+    const classes = useStyles();
+
+    var abstract = props.course.abstract
+    if (abstract.length > 80) {
+        abstract = abstract.substring(0, 80) + "..."
+    }
+    return (
+        <Card>
+            <CardActionArea>
+                <CardContent className={classes.title}>
+                    <h3>{props.course.name}</h3>
+                </CardContent>
+                <CardContent className={classes.abstract}>
                     <p>{abstract}</p>
                 </CardContent>
-            </Card>
-        )
-    }
+            </CardActionArea>
+        </Card>
+    )
 }
-
-export default CourseCard
