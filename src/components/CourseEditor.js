944ef2a9150d069157b633ab4885be1d00bf705a
oscar000803 <oscar000803@gmail.com>
2021-07-06 22:38:31 +0800

    Add course Editor CSS and finish button

--- src/components/CourseEditor.js
+++ src/components/CourseEditor.js
@@ -1,30 +1,124 @@
-import { Grid, Button, makeStyles, Typography, Popper, Paper, MenuItem, ClickAwayListener, MenuList, Link } from "@material-ui/core"
-import { Link as RouterLink } from 'react-router-dom';
+import { Grid, IconButton, makeStyles, Typography, Popper, Paper, MenuItem, ClickAwayListener, MenuList } from "@material-ui/core"
 import { useState } from "react";
 import CourseBlockEditor from "./CourseBlockEditor";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import AddIcon from '@material-ui/icons/Add';
 
-export default function CourseEditor(){
+const useStyles = makeStyles((theme) => ({
+    courseBlockEditor: {
+        margin: "20px 0 20px 0"
+    },
+    title: {
+        width: "99%"
+    }
+}))
 
+const TitleBLock = () => {
+
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    const [focus, setFocus] = useState(false)
     const [createBlockOptionConfig, setCreateBlockOptionConfig] = useState({
         open: false,
         anchor: null,
         placement: 'right-start'
     })
 
+    const handleClose = () => {
+        setCreateBlockOptionConfig({
+            open: false,
+            anchor: null,
+            placement: 'right-start'
+        })
+    }
+    return(
+        <ClickAwayListener 
+            onClickAway={()=>{
+                setFocus(false)
+                handleClose()
+            }
+        }>
+            <Grid container justify="center" alignItems="center" onClick={()=>setFocus(true)}>
+                {focus?
+                    <Grid item xs={1} className={classes.button_container}/>
+                    :
+                    <></>
+                }
+                <Grid item xs={10}>
+                    <Typography variant="h5">Title</Typography>
+                    {/* <input className={classes.title} defaultValue={"Course Title"}></input> */}
+                </Grid>
+                {focus?
+                    <Grid item xs={1} className={classes.button_container}>
+                        <IconButton 
+                            id={`addBlockButton_title`}
+                            onClick={(e)=>{
+                                setCreateBlockOptionConfig({
+                                    open: !createBlockOptionConfig.open,
+                                    anchor: document.getElementById(`addBlockButton_title`),
+                                    placement: 'right-start'
+                                })
+                            }
+                        }>
+                            <AddIcon />
+                        </IconButton>
+                        <Popper 
+                            open={createBlockOptionConfig.open}
+                            anchorEl={createBlockOptionConfig.anchor}
+                            placement={createBlockOptionConfig.placement}
+                            transition
+                        >
+                            <Paper>
+                                <ClickAwayListener 
+                                    onClickAway={()=>{
+                                        handleClose()
+                                    }
+                                }>
+                                    <MenuList>
+                                        <MenuItem onClick={(e)=>{
+                                            dispatch({ type: "ADD_NEW_BLOCK", payload: { index: -1 } })
+                                            handleClose()
+                                            setFocus(false)
+                                        }}>
+                                            新增文字
+                                        </MenuItem>
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Popper>
+                    </Grid>
+                    :
+                    <></>
+                }
+                
+            </Grid>
+        </ClickAwayListener>
+    )
+}
+
+export default function CourseEditor(){
+
+    const classes = useStyles()
+
     const blocks = useSelector(state => state.courseEditorReducer.blocks)
 
     return(
         <Grid container justify="center">
             <Grid item xs={10}>
+                <div className={classes.courseBlockEditor}>    
+                    <TitleBLock/>
+                </div>
                 {
                     blocks.map((block, index) => {
-                        return <CourseBlockEditor
-                            key={index}
-                            index={index}
-                            block={block}
-                        />
-                    })
+                        return(
+                            <div className={classes.courseBlockEditor}>
+                                <CourseBlockEditor
+                                    key={index}
+                                    index={index}
+                                    block={block}
+                                />
+                            </div> 
+                    )})
                 }
             </Grid>
         </Grid>
--- src/pages/EditCoursePages.js
+++ src/pages/EditCoursePages.js
@@ -1,12 +1,53 @@
 import NavBar from "../components/NavBar"
 import CourseEditor from "../components/CourseEditor"
+import { Grid, Button, makeStyles } from "@material-ui/core"
+import { Redirect } from "react-router"
+import { useState } from "react"
+
+
+const useStyles = makeStyles((theme) => ({
+    finishButton: {
+        background: "rgba(104, 144, 79, 0.9)"
+    }
+}))
 
 export default function EditCoursePage(props){
+
+    const classes = useStyles()
+
+    const [done, setDone] = useState(false)
 
     return(
         <div>
             <NavBar context="Bow-Code"/>
             <CourseEditor/>
+            <Grid container justify="center">
+                <Grid item xs={10}>
+                    <Grid container justify="center">
+                        <Grid item xs={10}>
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Button
+                                        onClick={(e) => {
+                                            setDone(true)
+                                        }}
+                                        variant="contained"
+                                        color="primary"
+                                        className={classes.finishButton}
+                                    >
+                                        完成
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            {done?
+                <Redirect to="/home"/>
+                :
+                <></>
+            }
         </div>
     )
 }
