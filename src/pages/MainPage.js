e321ebc1b2ca65c0125deb415f59317cb9493d7b
楊鈞智 <ken@yangjunzhide-MacBook-Pro.local>
2021-07-09 14:32:58 +0800

    revise .env to localhost url and delete some unused packages

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,9 @@
-import { Typography, List, ListItem, ListItemText, Link, makeStyles } from '@material-ui/core'
-import { shallowEqual, useSelector } from 'react-redux'
-import { Link as RouterLink } from 'react-router-dom';
 import NavBar from '../components/NavBar'
+import  React, { useState } from 'react'
 
 
 export default function MainPage() {
+
     return (
         <div>
             <NavBar context="Bow-Code" />
