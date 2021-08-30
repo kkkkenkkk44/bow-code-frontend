d9a38b8c9b4b1338972cad0b0ea2a6cb986d3cad
J-HowHuang <howardhuang2001@gmail.com>
2021-08-31 00:17:39 +0800

    Deadline setter of quiz

--- src/components/ClassroomTabs/QuizTile.js
+++ src/components/ClassroomTabs/QuizTile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Accordion } from '@material-ui/core'
+import { Accordion, IconButton } from '@material-ui/core'
 import { AccordionSummary, AccordionDetails } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
@@ -9,9 +9,60 @@ import { ListItem } from '@material-ui/core'
 import ListItemText from '@material-ui/core/ListItemText';
 import { Switch } from '@material-ui/core'
 import { Button } from '@material-ui/core'
+import EventIcon from '@material-ui/icons/Event';
+import { DatePicker } from "@material-ui/pickers";
+import { TimePicker } from "@material-ui/pickers";
+import { Dialog } from '@material-ui/core'
+import { DialogTitle } from '@material-ui/core'
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 function ListItemLink(props) {
     return <ListItem button component="a" {...props} />;
+}
+
+function DeadlineSetter(props) {
+    const { open, handleClose } = props
+    const [date, changeDate] = React.useState(new Date());
+    return <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} maxWidth={'80%'}>
+        <DialogTitle id="simple-dialog-title">變更截止日期</DialogTitle>
+        <div style={{ display: 'flex' }}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils} >
+                <DatePicker
+                    autoOk
+                    variant="static"
+                    openTo="date"
+                    value={date}
+                    onChange={changeDate}
+                />
+            </MuiPickersUtilsProvider>
+            <MuiPickersUtilsProvider utils={DateFnsUtils} >
+                <TimePicker
+                    autoOk
+                    variant="static"
+                    format="HH:mm:ss"
+                    views={["hours", "minutes", "seconds"]}
+                    openTo="hours"
+                    value={date}
+                    onChange={changeDate}
+                />
+            </MuiPickersUtilsProvider>
+        </div>
+        <div style={{
+            marginTop: 'auto',
+            marginRight: '20px',
+            marginBottom: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+        }}>
+            <Button variant="contained" color="primary" onClick={() => { }}>變更</Button>
+        </div>
+    </Dialog>
 }
 
 export default function QuizTile(props) {
@@ -40,7 +91,9 @@ export default function QuizTile(props) {
         },
         deadline: {
             flex: 1,
-            zIndex: 10
+            zIndex: 11,
+            display: 'flex',
+            alignItems: 'center'
         },
         problemList: {
             zIndex: 10
@@ -74,6 +127,7 @@ export default function QuizTile(props) {
         }
     }));
     const classes = useStyles()
+    const [open, setOpen] = React.useState(false);
 
     const problemList = quiz.problems.map(problem => <ListItemLink className={classes.content} target="_blank" rel="noopener noreferrer" href={`/problem/${problem.problemId}`}>
         <ListItemText primary={problem.name} />
@@ -99,9 +153,13 @@ export default function QuizTile(props) {
                 <Typography className={classes.title}>{quiz.title}</Typography>
                 {isRecord && <Typography className={classes.score} variant="h5">{quiz.score}/{quiz.total}</Typography>}
                 {isRecord && <div className={classes.gradingDisplay}></div>}
-                {due ? <Typography className={classes.deadline} variant="subtitle1">{`已截止`}</Typography> :
-                    <Typography className={classes.deadline} variant="subtitle1">{`剩下 ${remainText}`}</Typography>
-                }
+                <div className={classes.deadline}>
+                    {due ? <Typography variant="subtitle1">{`已截止`}</Typography> :
+                        <Typography className={classes.deadline} variant="subtitle1">{`剩下 ${remainText}`}</Typography>
+                    }
+                    {!isRecord && <IconButton onClick={() => { setOpen(true) }} style={{ maxHeight: '40px', maxWidth: '40px', zIndex: 20 }}><EventIcon></EventIcon></IconButton>}
+                </div>
+                <DeadlineSetter open={open} handleClose={() => { setOpen(false) }}></DeadlineSetter>
             </AccordionSummary>
             {isRecord ?
                 <AccordionDetails className={classes.detail}>
@@ -114,7 +172,7 @@ export default function QuizTile(props) {
                         {problemList}
                     </List>
                     <div className={classes.actions}>
-                        <div style={{display: 'flex', alignItems: 'center'}}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
                             <Switch
                                 checked={quiz.public}
                                 onChange={() => { }}
