7658b1c55f079ab1bf93b2a6a211fea02771c895
oscar000803 <oscar000803@gmail.com>
2021-09-06 21:23:27 +0800

    now coursePlanEditorPage will render the componentList of the course plan

--- src/pages/CoursePlanEditorPage.js
+++ src/pages/CoursePlanEditorPage.js
@@ -1,40 +1,115 @@
 import React, { useEffect } from 'react'
 import NavBar from "../components/NavBar"
-import { Grid, Button, makeStyles } from "@material-ui/core"
+import { Grid, Button, makeStyles, CircularProgress } from "@material-ui/core"
 import { Redirect } from "react-router"
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import AddComponentButton from '../components/CreateCoursePlan/AddComponentButton';
 import { useParams } from 'react-router-dom'
+import CourseCard from '../components/CourseCard'
+import ComponentCard from '../components/ComponentCard'
+import ProblemListTile from '../components/ProblemListTile'
 
 const useStyles = makeStyles((theme) => ({
-    finishButton: {
-        background: "rgba(104, 144, 79, 0.9)",
-        margin: "20px 0 20px 0"
+    root: {
+        marginTop: theme.spacing(3)
+    },
+    componentCard: {
+        marginBottom: theme.spacing(2)
     }
 }))
 
 export default function CoursePlanEditorPage(props) {
 
     const { CoursePlanID } = useParams()
+    const classes = useStyles()
+    const { isFetching, componentList, componentDetailList } = useSelector(state => state.coursePlanEditorReducer)
     const dispatch = useDispatch()
-    console.log(CoursePlanID)
 
     useEffect(() => {
+        dispatch({ type: "FETCHING_COURSEPLAN" })
         fetch(`${process.env.REACT_APP_BACKEND_URL}/course_plan/${CoursePlanID}`, {
             method: 'GET',
             credentials: "include"
         })
             .then(res => res.json())
-            .then(res =>
+            .then(res => {
                 dispatch({ type: "SAVE_COURSEPLAN_INFO", payload: res })
-            )
+                console.log(res)
+                // Promise.all(res.componentList.map(component => {
+                //     switch (component.type) {
+                //         case 0:
+                //             return fetch(`${process.env.REACT_APP_BACKEND_URL}/course/details?courses=${component.setList[0]}`, {
+                //                 method: 'GET',
+                //                 credentials: 'include',
+                //             })
+                //     }
+                // }))
+                //     .then(res => {
+                //         Promise.all(res.map(r => r.text()))
+                //             .then(res => console.log(res))
+                //     })
+            })
+        // fetch(`${process.env.REACT_APP_BACKEND_URL}/course/details?courses=612c9f38255e0aa1fa29b220`, {
+        //     method: 'GET',
+        //     credentials: 'include',
+        // })
+        //     .then(res => res.json())
+        //     .then(res => console.log(res))
     }, [])
+
+    var componentCardList = []
+    componentCardList = componentDetailList.map(component => {
+        if (component.type === 0) {
+            return <div className={classes.componentCard}><CourseCard course={component.setList[0]} unclickable /></div>
+        }
+        else if (component.type === 1) {
+            return (
+                <div className={classes.componentCard}>
+                    <ComponentCard name={component.name} totalScore={component.totalScore}>
+                        {
+                            component.setList.map(problem => {
+                                return <ProblemListTile problem={problem} isPicker={false} />
+                            })
+                        }
+                    </ComponentCard>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className={classes.componentCard}>
+                    <ComponentCard name={component.name} totalScore={component.totalScore}>
+                        {
+                            component.setList.map(problem => {
+                                return <ProblemListTile problem={problem} isPicker={false} />
+                            })
+                        }
+                    </ComponentCard>
+                </div>
+            )
+        }
+    })
 
     return (
         <div>
             <NavBar context="Bow-Code" />
-            <AddComponentButton />
+            <div className={classes.root}>
+                <Grid container>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={8}>
+                        {componentCardList}
+                        {isFetching ?
+                            <CircularProgress />
+                            :
+                            <div></div>
+                        }
+                    </Grid>
+                    <Grid item xs={2}>
+                        <AddComponentButton />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 
